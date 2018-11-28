eb0203548996ef1e1dfd5d376837d3db5c18eb86
smlparry <samuel.parry@bigpond.com>
2018-11-28 11:49:55 +1100

    Support opening external links

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import BrowserWindow from 'sketch-module-web-view'
-import Settings from 'sketch/settings'
-import { Document } from 'sketch/dom'
+import BrowserWindow from "sketch-module-web-view";
+import Settings from "sketch/settings";
+import { Document } from "sketch/dom";
 
-import importRemoteFile from '../resources/utils/importRemoteFile'
+import importRemoteFile from "../resources/utils/importRemoteFile";
 
 export default function(context) {
-  const document = Document.getSelectedDocument()
-  const documentId = document.id
+  const document = Document.getSelectedDocument();
+  const documentId = document.id;
 
   const options = {
     identifier: documentId,
@@ -15,43 +15,47 @@ export default function(context) {
     width: 900,
     height: 700,
     minWidth: 400,
-    minHeight: 400,
-  }
+    minHeight: 400
+  };
 
   // Setup webview
-  var browserWindow = new BrowserWindow(options)
+  var browserWindow = new BrowserWindow(options);
 
-  browserWindow.once('ready-to-show', () => {
-    browserWindow.show()
-  })
+  browserWindow.once("ready-to-show", () => {
+    browserWindow.show();
+  });
 
-  const webContents = browserWindow.webContents
+  const webContents = browserWindow.webContents;
 
   // Connect To Webview
-  webContents.on('connectToSketch', () => {
-    webContents.executeJavaScript(`sketchConnected('${documentId}')`)
+  webContents.on("connectToSketch", () => {
+    webContents.executeJavaScript(`sketchConnected('${documentId}')`);
 
-    const licenseCode = Settings.settingForKey('license_code')
-    const licenseEmail = Settings.settingForKey('license_email')
+    const licenseCode = Settings.settingForKey("license_code");
+    const licenseEmail = Settings.settingForKey("license_email");
     webContents.executeJavaScript(
       `setLicense('${licenseCode}', '${licenseEmail}')`
-    )
-  })
+    );
+  });
 
   // Set Api Key
-  webContents.on('setLicense', (licenseCode, licenseEmail) => {
-    Settings.setSettingForKey('license_code', licenseCode)
-    Settings.setSettingForKey('license_email', licenseEmail)
+  webContents.on("setLicense", (licenseCode, licenseEmail) => {
+    Settings.setSettingForKey("license_code", licenseCode);
+    Settings.setSettingForKey("license_email", licenseEmail);
 
     webContents.executeJavaScript(
       `setLicense('${licenseCode}', '${licenseEmail}')`
-    )
-  })
+    );
+  });
 
-  webContents.on('openFile', base64Data =>
+  webContents.on("openFile", base64Data =>
     importRemoteFile(document, base64Data)
-  )
+  );
+
+  webContents.on("openExternalLink", url =>
+    NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString(url))
+  );
 
   // Load webview
-  browserWindow.loadURL('http://192.168.0.18:5000/')
+  browserWindow.loadURL("http://192.168.0.18:5000/");
 }

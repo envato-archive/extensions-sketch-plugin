60595b782b5fe6637b33718f587cd36859f8e0dc
smlparry <samuel.parry@bigpond.com>
2018-12-18 12:07:31 +1100

    Set ENV when building and developing

--- webpack.skpm.config.js
+++ webpack.skpm.config.js
@@ -1,28 +1,31 @@
+const webpack = require("webpack");
+
 module.exports = function(config, isPluginCommand) {
+  config.plugins.push(new webpack.EnvironmentPlugin(["ENV"]));
   config.module.rules.push({
     test: /\.(html)$/,
     use: [
       {
-        loader: '@skpm/extract-loader',
+        loader: "@skpm/extract-loader"
       },
       {
-        loader: 'html-loader',
+        loader: "html-loader",
         options: {
-          attrs: ['img:src', 'link:href'],
-          interpolate: true,
-        },
-      },
-    ],
-  })
+          attrs: ["img:src", "link:href"],
+          interpolate: true
+        }
+      }
+    ]
+  });
   config.module.rules.push({
     test: /\.(css)$/,
     use: [
       {
-        loader: '@skpm/extract-loader',
+        loader: "@skpm/extract-loader"
       },
       {
-        loader: 'css-loader',
-      },
-    ],
-  })
-}
+        loader: "css-loader"
+      }
+    ]
+  });
+};

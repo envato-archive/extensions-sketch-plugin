32caaeae78e01b1d1c2b6ac391f28ca442bbc09a
smlparry <samuel.parry@bigpond.com>
2019-02-27 11:49:54 +1100

    Copy logo to resources folder

--- webpack.skpm.config.js
+++ webpack.skpm.config.js
@@ -1,7 +1,20 @@
+const path = require("path");
 const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { skpm } = require("./package.json");
+
+const pluginResourcesPath = `${skpm.main}/Contents/Resources`;
 
 module.exports = function(config, isPluginCommand) {
   config.plugins.push(new webpack.EnvironmentPlugin(["ENV"]));
+  config.plugins.push(
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve("./resources/icon.png"),
+        to: path.resolve(pluginResourcesPath)
+      }
+    ])
+  );
   config.module.rules.push({
     test: /\.(html)$/,
     use: [

4e4631fe0b71ba1986a257a7d94c6ba032f01833
smlparry <samuel.parry@bigpond.com>
2019-05-08 11:12:37 +1000

    Fix bug when there no artboards on the page

--- resources/utils/calculateImportCoordinates.js
+++ resources/utils/calculateImportCoordinates.js
@@ -1,6 +1,7 @@
 export default document => {
   let lastArtboard = null;
   let largestHypotenuse = 0;
+  let coordinates = { x: 0, y: 0 };
 
   document.layers.forEach(layer => {
     // Calculate Hypotenuse from origin
@@ -8,14 +9,22 @@ export default document => {
       Math.pow(layer.frame.y, 2) + Math.pow(layer.frame.x, 2)
     );
 
-    if (layerOffset > largestHypotenuse) {
+    console.log(layer);
+
+    if (!lastArtboard) {
+      lastArtboard = layer;
+    } else if (layerOffset > largestHypotenuse) {
       largestHypotenuse = layerOffset;
       lastArtboard = layer;
     }
   });
 
-  return {
-    x: lastArtboard.frame.x + lastArtboard.frame.width + 80,
-    y: lastArtboard.frame.y
-  };
+  if (lastArtboard) {
+    coordinates = {
+      x: lastArtboard.frame.x + lastArtboard.frame.width + 80,
+      y: lastArtboard.frame.y
+    };
+  }
+
+  return coordinates;
 };

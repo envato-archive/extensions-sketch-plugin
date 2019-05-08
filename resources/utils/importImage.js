679a7030b425cad134a83527b955764b7720e0ca
smlparry <samuel.parry@bigpond.com>
2019-05-08 10:52:10 +1000

    Calculate and import to correct coordinates from importImage

--- resources/utils/importImage.js
+++ resources/utils/importImage.js
@@ -2,6 +2,7 @@ import os from "os";
 import path from "path";
 import fs from "@skpm/fs";
 import { Artboard, Image } from "sketch/dom";
+import calculateImportCoordinates from "./calculateImportCoordinates";
 
 const FOLDER = path.join(os.tmpdir(), "com.sketchapp.envato-sketch-plugin");
 const IMPORT_IMAGE_WIDTH = 1000;
@@ -18,17 +19,22 @@ export default (parentDocument, data) => {
   }
   try {
     fs.writeFileSync(outputPath, buffer, "NSData");
-
     const heightFactor = IMPORT_IMAGE_WIDTH / data.width;
 
     const height = data.height * heightFactor;
     const width = IMPORT_IMAGE_WIDTH;
-
     const importPage = parentDocument.selectedPage || parentDocument.pages[0];
+
+    const coordinates = calculateImportCoordinates(importPage);
 
     const artboard = new Artboard({
       parent: importPage,
-      frame: { width, height }
+      frame: {
+        x: coordinates.x,
+        y: coordinates.y,
+        width,
+        height
+      }
     });
     new Image({
       image: outputPath,
